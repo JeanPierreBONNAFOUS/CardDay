4b2a2482ab28fa48109f22da00cd3f7ee51c4b64
Jean Pierre <jeanpierre.bonnafous@gmail.com>
2019-11-11 06:28:42 +0000

    On reprend le contrôle

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 # Une liste de tuple
+# Ce script est sous controle de version
 from Ext import a, augment
+# On charge le fichier Evt.txt
 E=[]
 with open("Evt.txt","r") as fichier:
   E=fichier.readlines()
-Text="strange quote"
+
 print(E[-1])
 print(a)
 print(augment(2))
 
-M="12345"
+M="123456"
 j=0
 print("i,j,M")
+# on joue avec la focntion %
 for i in range(10):
   if i%len(M) ==0:
     j=0
